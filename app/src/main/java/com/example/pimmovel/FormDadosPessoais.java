f4421460d155cf0abc43e86b567d9f4eeb37ec05
JPvazzZ <90486714+JPvazzZ@users.noreply.github.com>
2022-12-07 20:36:07 -0300

    dream_life_mobile_3.0

--- app/src/main/java/com/example/pimmovel/FormDadosPessoais.java
+++ app/src/main/java/com/example/pimmovel/FormDadosPessoais.java
@@ -2,9 +2,16 @@ package com.example.pimmovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FormDadosPessoais extends AppCompatActivity {
+
+    private TextView textNomeUsuario,textEmailUsuario;
+    private Button bt_deslogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,26 @@ public class FormDadosPessoais extends AppCompatActivity {
         setContentView(R.layout.activity_form_dados_pessoais);
 
         getSupportActionBar().hide();
+
+        iniciarComponentes();
+
+        bt_deslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(FormDadosPessoais.this,FormLogin.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+
+    private void iniciarComponentes(){
+
+    textNomeUsuario = findViewById(R.id.textNomeUsuario);
+    textEmailUsuario = findViewById(R.id.textEmailUsuario);
+    bt_deslogar = findViewById(R.id.bt_deslogar);
     }
 }
--- app/src/main/java/com/example/pimmovel/FormLogin.java
+++ app/src/main/java/com/example/pimmovel/FormLogin.java
@@ -2,9 +2,20 @@ package com.example.pimmovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class FormLogin extends AppCompatActivity {
+
+    private Button button_tela_dadospessoais;
+    private EditText edit_email,edit_senha;
+    String[] mensagens = {"Prencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,41 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+
+        iniciarComponentes();
+
+        button_tela_dadospessoais.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+
+                    Intent intent = new Intent(FormLogin.this,FormDadosPessoais.class);
+                    startActivity(intent);
+
+                }
+
+
+
+            }
+        });
+
+    }
+
+        private void iniciarComponentes(){
+            button_tela_dadospessoais = findViewById(R.id.bt_entrar);
+            edit_email = findViewById(R.id.edit_email);
+            edit_senha = findViewById(R.id.edit_eye);
+
+
+
     }
 }
